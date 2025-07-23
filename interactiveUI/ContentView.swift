87648f112b5db81fe1961f010237917b64e8bb3c
Chaitra Sure <chaitrasure@gmail.com>
2025-07-23 14:39:16 -0700

    added text field with modifications like alignment and border

--- interactiveUI/ContentView.swift
+++ interactiveUI/ContentView.swift
@@ -16,6 +16,12 @@ struct ContentView: View {
                 Text("What is your name?")
                     .fontWeight(.bold)
                     .font(.system(size: 40))
+                TextField("Type your name here", text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant("")/*@END_MENU_TOKEN@*/)
+                    .multilineTextAlignment(.center)
+                    .font(.title)
+                    .border(Color.gray, width: 1)
+                
+                    .padding()
         }
         
         }
